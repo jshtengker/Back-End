523e040f054eacbe94646c06d18a9ad8e5e5fa0c
jshtengker <leonardotengker@gmail.com>
2024-03-27 21:00:11 +0800

    postgre

--- index.js
+++ index.js
@@ -1,114 +1,71 @@
+/* ---------------- */
 // app.js
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-const users = require('./users');
+const db = require('./db')
 
 const app = express();
 const port = 3000;
 
+
 // Middlewares
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(cors({origin: 'http://127.0.0.1:5500'}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Setup storage for file uploads
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads'); // Set destination folder
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + file.originalname);
-  }
-});
-const upload = multer({ storage: storage });
 
-// Routes
-// GET: /users
-app.get('/users', (req, res) => {
-  res.json(users);
-});
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
-// GET: /users/:name
-app.get('/users/:name', (req, res) => {
-  const userName = req.params.name.toLowerCase();
-  const user = users.find(u => u.name.toLowerCase() === userName);
-  if (!user) {
-    return res.status(404).json({status: "error", message: "Data user tidak ditemukan"});
-  }
-  res.json(user);
-});
-
-// POST: /users
-app.post('/users', (req, res) => {
-  const { id, name } = req.body;
-  if (!id || !name) {
-    return res.status(400).json({
-      status: 'error',
-      message: 'Id and name are required'
+app.get("/students", async (req, res) => {
+  try {
+    // const result = await db.query("SELECT * FROM students");
+    const allStudents = await prisma.students.findMany();
+    res.status(200).json({
+      status: "success",
+      data: allStudents,
     });
-  }
-  const newUser = {
-    id,
-    name
-  };
-  users.push(newUser);
-
-  res.status(201).json({
-    status: 'success',
-    data: newUser
-  });
-});
-
-// POST: /upload
-app.post('/upload', upload.single('file'), (req, res) => {
-  if (req.file) {
-    res.send('File uploaded successfully');
-  } else {
-    res.send('Error uploading file');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.put('/users/:name', (req, res) => {
-  const { name } = req.params;
-  const { newName } = req.body;
-  if (!newName) {
-    return res.status(400).json({ status: "error", message: "Masukan data yang akan di input" });
+app.post("/students", async (req, res) => {
+  const { name, address } = req.body;
+  try {
+    // const result = await db.query(
+    //   `INSERT into students (name, address) values ('${name}', '${address}')`
+    // );
+    await prisma.students.create({
+      data: {
+        name: name,
+        address: address,
+      }
+    });
+    res.status(200).json({
+      status: "success",
+      message: "data berhasil dimasukan",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
   }
-  const userIndex = users.findIndex(u => u.name.toLowerCase() === name.toLowerCase());
-  if (userIndex === -1) {
-    return res.status(404).json({ status: "error", message: "Data user tidak ditemukan" });
-  }
-  users[userIndex].name = newName;
-  res.json(users[userIndex]);
 });
 
-app.delete('/users/:name', (req, res) => {
-  const { name } = req.params;
-  const index = users.findIndex(u => u.name.toLowerCase() === name.toLowerCase());
-  if (index === -1) {
-    return res.status(404).json({ status: "error", message: "Data user tidak ditemukan" });
-  }
-  users.splice(index, 1);
-  res.status(204).json({ status: "success", message: "Data user berhasil dihapus" });
-});
+// Update Student by ID
 
-// 404 Route
-app.use((req, res) => {
-  res.status(404).json({status: "error", message: "resource tidak ditemukan"});
-});
 
-// Error handling Middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({status: "error", message: "terjadi kesalahan pada server"});
-});
+// Delete Student by ID
+
+// Get student by ID
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+/* ----------------- */
